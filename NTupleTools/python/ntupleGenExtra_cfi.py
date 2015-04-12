a40360f3acea018460e006a6465ef3a0f1a94b22
Jia Fu Low <jia.fu.low@cern.ch>
2015-04-12 08:49:00 -0500

    Renamed BeamSpotFromSim -> simBeamSpot

--- NTupleTools/python/ntupleGenExtra_cfi.py
+++ NTupleTools/python/ntupleGenExtra_cfi.py
@@ -1,17 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 #from SLHCL1TrackTriggerSimulations.NTupleTools.prunedGenParticles_cfi import prunedGenParticles
-from SLHCL1TrackTriggerSimulations.NTupleTools.BeamSpotFromSim_cfi import BeamSpotFromSim
+from SLHCL1TrackTriggerSimulations.NTupleTools.simBeamSpot_cfi import simBeamSpot
 
 ntupleGenParticlesExtra = cms.EDProducer('NTupleGenParticlesExtra',
     #inputTag = cms.InputTag('prunedGenParticles'),
     inputTag = cms.InputTag('genParticles'),
-    inputTagBeamSpot = cms.InputTag('BeamSpotFromSim', 'BeamSpot'),
+    inputTagBeamSpot = cms.InputTag('simBeamSpot', 'BeamSpot'),
     prefix = cms.string('genParts@'),
     suffix = cms.string(''),
     cut = cms.string(''),
     maxN = cms.uint32(999999)
 )
 
-ntupleGenExtra = cms.Sequence(BeamSpotFromSim * ntupleGenParticlesExtra)
+ntupleGenExtra = cms.Sequence(simBeamSpot * ntupleGenParticlesExtra)
 
