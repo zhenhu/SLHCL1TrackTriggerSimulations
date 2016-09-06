2a3f3576cab9cb31653e43229c76252ff5209cbb
Zhen <zhen.hu@cern.ch>
2016-09-06 15:30:25 -0500

    use static const to save memory

--- AMSimulation/interface/Helper.h
+++ AMSimulation/interface/Helper.h
@@ -70,8 +70,8 @@ inline unsigned compressLayer(const unsigned& lay) {
 inline bool stubDeltaSFilter(unsigned moduleId, float bend) {
    unsigned lay = decodeLayer(moduleId); 
    unsigned lad = decodeLadder(moduleId); 
-   float Barrel[6] = {1.5, 1.5, 2.5, 4, 5.5, 6.5};
-   float Endcap[5][15] = { 
+   static const float Barrel[6] = {1.5, 1.5, 2.5, 4, 5.5, 6.5};
+   static const float Endcap[5][15] = { 
    {1.0, 1.0, 1.5, 2.0, 2.5, 2.5, 2.5, 3.0, 3.5, 4.5, 3.0, 3.5, 4.0, 4.5, 5.0},  //D1
    {1.0, 1.0, 1.5, 2.0, 2.0, 2.5, 2.5, 2.5, 3.0, 4.0, 2.5, 3.0, 3.5, 4.0, 4.5},  //D2
    {1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.5, 2.5, 2.5, 3.5, 4.0, 2.5, 3.0, 3.5, 4.0},  //D3
